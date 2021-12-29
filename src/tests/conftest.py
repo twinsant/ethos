bd8a076448451934b6d5b5439aa849271c6a4a52
twinsant <twinsant@gmail.com>
2021-12-29 10:06:02 +0800

    Add: pytest codebase.

---
+++ src/tests/conftest.py
@@ -0,0 +1,30 @@
+import os
+import tempfile
+
+import pytest
+from ethos import create_app
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    # TODO
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
---
+++ src/tests/test_factory.py
@@ -0,0 +1,11 @@
+from ethos import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, Web3.0!'
