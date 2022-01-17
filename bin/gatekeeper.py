5679c3d2f40a6f49d93aee8aba71484b4466ea01
twinsant <twinsant@gmail.com>
2022-01-17 11:01:22 +0800

    Dbg: Chrome close the socket?

--- bin/gatekeeper.py
+++ bin/gatekeeper.py
@@ -1,30 +1,20 @@
-from geventwebsocket.handler import WebSocketHandler
-from geventwebsocket import WebSocketError
-from gevent.pywsgi import WSGIServer
-from flask import Flask, request
+from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
+from collections import OrderedDict
+from flask import Flask
 
-app = Flask(__name__)
+class EchoApplication(WebSocketApplication):
+    def on_open(self):
+        print("Connection opened")
 
-@app.route('/')
-def index():
-    return 'Hello'
+    def on_message(self, message):
+        if not self.ws.closed:
+            self.ws.send(message)
 
-# https://github.com/jgelens/gevent-websocket/blob/master/geventwebsocket/handler.py
-@app.route('/ws')
-def ws():
-    if request.environ.get('wsgi.websocket'):
-        ws = request.environ['wsgi.websocket']
+    def on_close(self, reason):
+        print(reason)
 
-        closed = False
-        while not closed:
-            try:
-                message = ws.receive()
-                ws.send(message)
-            except WebSocketError as e:
-                print('Maybe closed', e, dir(ws))
-                closed = True
-    return ''
+app = EchoApplication(__name__)
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('127.0.0.1',4001), app, handler_class=WebSocketHandler)
+    http_server = WebSocketServer(('127.0.0.1',4001), Resource(OrderedDict([('/', EchoApplication)])), debug=True)
     http_server.serve_forever()
