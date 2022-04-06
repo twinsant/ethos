90054051faf4f086654e58ad68c49ba2f2d1c01f
twinsant <twinsant@gmail.com>
2022-04-06 09:51:17 +0800

    Add: payble setGreeter poc.

--- src/ethos/static/js/contracts.js
+++ src/ethos/static/js/contracts.js
@@ -1,4 +1,43 @@
 let contracts = {
+    "PaidGreeter": [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_greeting",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "greet",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_greeting",
+          "type": "string"
+        }
+      ],
+      "name": "setGreeting",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+  ],
     "Greeter": [
         {
             "inputs": [
