23f1d131f0179fb5361e407b974dbbbce75d819f
twinsant <twinsant@gmail.com>
2022-04-24 09:33:18 +0800

    Chg: refactoring core npc object

--- lib/system/metaverse/npc/rabbit.c
+++ lib/system/metaverse/npc/rabbit.c
@@ -1,24 +1,4 @@
-inherit CORE_LIVING;
-
-int id(string arg)
-{
-    if (strcmp(capitalize(arg), query("id")) == 0){
-        return 1;
-    }
-    return 0;
-}
-
-string get_id()
-{
-    string _id = "";
-    string fn = file_name(this_object());
-
-    int n = strsrch(fn, "/", -1);
-    if (n!=-1) {
-        _id = capitalize(fn[n+1..]);
-    }
-    return _id;
-}
+inherit CORE_NPC;
 
 void create()
 {
